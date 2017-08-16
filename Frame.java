bf3ad7a4602f6073d75ff6a703a78b46421c1156
Andrew Johnston <adjohnston99@gmail.com>
2017-08-15 21:46:11 -0700

    Refactoring GUI
    Replacing JFrame GUI with JavaFX

--- Frame.java
+++ Frame.java
@@ -1,29 +1,23 @@
 package Vigeniere;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class Frame extends JFrame implements ActionListener, FocusListener {
-
-	private static final long serialVersionUID = 1L;
+public class Frame extends Application implements EventHandler<ActionEvent> {
 	
-	JTextArea input = new JTextArea("Input");
-	JTextArea key = new JTextArea("Key");
-	JTextArea output = new JTextArea("Output");
-	
-	JButton encryptButton = new JButton("Encrypt");
-	JButton decryptButton = new JButton("Decrypt");
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
 	
 	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 	int screenWidth = gd.getDisplayMode().getWidth();
@@ -38,70 +32,107 @@ public class Frame extends JFrame implements ActionListener, FocusListener {
 	double defaultFontSize = screenHeight*50/100; 
 	Font defaultFont = new Font("SansSerif", Font.BOLD, (int) defaultFontSize);
 	
-	Frame() {
+	Frame(String[] args) {
 		
-		System.out.println(screenWidth + ", " + screenHeight);
-		System.out.println(frameWidth + ", " + frameHeight);
-		System.out.println(framePosX + ", " + framePosY);
+		launch(args);
 		
-		encryptButton.addActionListener(this);
-		decryptButton.addActionListener(this);
-		
-		input.addFocusListener(this);
-		key.addFocusListener(this);
-		output.addFocusListener(this);
-		
-		input.setPreferredSize(new Dimension(screenWidth*5/100, screenHeight*5/100));
-		
-		add(input);
-		add(key);
-		add(output);
-		
-		add(encryptButton);
-		add(decryptButton);
-		
-		
-		setLayout(new FlowLayout());
-		setSize((int) frameWidth, (int) frameHeight);
-		setLocation((int) framePosX, (int) framePosY);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
+//		System.out.println(screenWidth + ", " + screenHeight);
+//		System.out.println(frameWidth + ", " + frameHeight);
+//		System.out.println(framePosX + ", " + framePosY);
+//		
+//		encryptButton.addActionListener(this);
+//		decryptButton.addActionListener(this);
+//		
+//		input.addFocusListener(this);
+//		key.addFocusListener(this);
+//		output.addFocusListener(this);
+//		
+//		input.setPreferredSize(new Dimension(screenWidth*5/100, screenHeight*5/100));
+//		
+//		add(input);
+//		add(key);
+//		add(output);		
+//		
+//		setLayout(new FlowLayout());
+//		setSize((int) frameWidth, (int) frameHeight);
+//		setLocation((int) framePosX, (int) framePosY);
+//		setDefaultCloseOperation(EXIT_ON_CLOSE);
+//		setVisible(true);
 	}
 
+	FlowPane componentLayout = new FlowPane();
+	
+	TextField input;
+	TextField key;
+	TextField output;
+	
+	Button encryptButton;
+	Button decryptButton;
+	
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void start(Stage primaryStage) throws Exception {
 		
+		primaryStage.setTitle("Vigeniere");
+		
+		input = new TextField("Input");
+		key = new TextField("Key");
+		output = new TextField("Output");
+		
+		encryptButton = new Button("Encrypt");
+		decryptButton = new Button("Decrypt");
+		
+		encryptButton.addEventHandler(ActionEvent.ACTION, this);
+		decryptButton.addEventHandler(ActionEvent.ACTION, this);
+		
+		componentLayout.getChildren().add(input);
+		componentLayout.getChildren().add(key);
+		componentLayout.getChildren().add(output);
+		
+		componentLayout.getChildren().add(encryptButton);
+		componentLayout.getChildren().add(decryptButton);
+		
+		Scene appScene = new Scene(componentLayout, 500, 500);
+		
+		primaryStage.setScene(appScene);
+		primaryStage.show();
+	}
+	
+	public void handle(ActionEvent event) {
+	
 		String inputText = input.getText();
 		String keyText = key.getText();
 		
-		if (e.getSource() == encryptButton) {
+		if (event.getSource() == encryptButton) {
 			output.setText(Algorithms.encrypt(inputText, keyText));
-		} else if (e.getSource() == decryptButton) {
+		} else if (event.getSource() == decryptButton) {
 			output.setText(Algorithms.decrypt(inputText, keyText));
-		}		
+		}
+		
+		event.consume();
 	}
 
-	@Override
-	public void focusGained(FocusEvent e) {
-		
-		if ((e.getSource() == input) && (input.getText().equals("Input"))) {
-			input.setText("");
-		} else if ((e.getSource() == key) && (key.getText().equals("Key"))) {
-			key.setText("");
-		} else if ((e.getSource() == output) && (output.getText().equals("Output"))) {
-			output.setText("");
-		}
-	}
-
-	@Override
-	public void focusLost(FocusEvent e) {
-		
-		if ((e.getSource() == input) && (input.getText().equals(""))) {
-			input.setText("Input");
-		} else if ((e.getSource() == key) && (key.getText().equals(""))) {
-			key.setText("Key");
-		} else if ((e.getSource() == output) && (output.getText().equals(""))) {
-			output.setText("Output");
-		}
-	}
+	
+//	@Override
+//	public void focusGained(FocusEvent e) {
+//		
+//		if ((e.getSource() == input) && (input.getText().equals("Input"))) {
+//			input.setText("");
+//		} else if ((e.getSource() == key) && (key.getText().equals("Key"))) {
+//			key.setText("");
+//		} else if ((e.getSource() == output) && (output.getText().equals("Output"))) {
+//			output.setText("");
+//		}
+//	}
+//
+//	@Override
+//	public void focusLost(FocusEvent e) {
+//		
+//		if ((e.getSource() == input) && (input.getText().equals(""))) {
+//			input.setText("Input");
+//		} else if ((e.getSource() == key) && (key.getText().equals(""))) {
+//			key.setText("Key");
+//		} else if ((e.getSource() == output) && (output.getText().equals(""))) {
+//			output.setText("Output");
+//		}
+//	}
 }
--- Main.java
+++ Main.java
@@ -14,6 +14,6 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		new Frame();
+		new Frame(args);
 	}
 }
